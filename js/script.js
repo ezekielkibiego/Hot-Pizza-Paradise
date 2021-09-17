98b09f7f1a9634213d20a7a35f8f30df4451ecb8
ezekielkibiego <ezekiel.nyambane@student.moringaschool.com>
2021-09-17 17:52:44 +0300

    add header

--- js/script.js
+++ js/script.js
@@ -10,3 +10,10 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+// welcome to Tomato's Pizzaria v2.0 with SASS
+const button = document.querySelector('.nav-button');
+const menu = document.querySelector('nav');
+button.addEventListener('click', () => {
+  menu.classList.toggle('visible');
+});
